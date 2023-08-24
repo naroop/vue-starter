a3d35150dd19ec1c29b6e8864030f54c6dd0518a
naroop <76455626+naroop@users.noreply.github.com>
2023-08-24 16:55:20 -0500

    minor fixes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vue-project
+# vue-starter
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import BlankView from '../views/BlankView.vue';
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: BlankView
     }
   ]
 });
